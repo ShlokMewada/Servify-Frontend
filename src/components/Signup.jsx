2010a2ff54997222f0f490b548663f6af46af6f8
ShlokMewada <shlokmewada@gmail.com>
2024-09-20 13:39:17 +0530

    signup styling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -89,7 +89,7 @@ const Signup = ({ isEmployee }) => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8 md:py-10">
       <form
         className="w-full max-w-md p-8 bg-white shadow-2xl rounded-lg space-y-6"
         onSubmit={(e) => {
